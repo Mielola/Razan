8155bc99b3f6af7aa6f01208f9755c13d2e616d0
Muhammad Wildan Dhiya Ulhaq <114896642+Mielola@users.noreply.github.com>
2022-10-09 13:20:32 +0700

    Add files via upload

---
+++ Jurnal01.java
@@ -0,0 +1,13 @@
+public class Jurnal01 {
+
+	public static void main(final String[] args) {
+		// Kerjakan soalnya di sini
+		String nama = ("Rabel");
+		int umur_abel = 21;
+		int umur_razan = 5;
+		int umur = umur_abel % umur_razan;
+
+		System.out.println("Halo" + nama + ".\n");
+		System.out.println("Ternyata usia rabel " + umur + " syaa!");
+	}
+}
